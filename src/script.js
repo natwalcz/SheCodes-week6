2ab9ce65e4ef2c4b0ce58c6a8d8ca9426c21e0b0
natwalcz <natalia.walczak@g.pl>
2023-01-05 13:51:34 +0100

    Added functions changing background and icons depending on weather.

--- src/script.js
+++ src/script.js
@@ -7,36 +7,81 @@
         if (http.status != 404)
             return true;
         else
-            return false;      
-    }
+            return false;
+      }
+
+  function changeIcon(response) {
+        let icon = document.querySelector("#icon");
+        if (response.data.weather[0].id > 199 && response.data.weather[0].id< 600)
+        {icon.innerHTML= `<img src="img/icon200.png" height= "58px">`}
+        if (response.data.weather[0].id > 599 && response.data.weather[0].id< 700)
+        {icon.innerHTML= `<img src="img/icon600.png" height= "58px">`}
+          if (response.data.weather[0].id > 699 && response.data.weather[0].id< 800)
+        {icon.innerHTML= `<img src="img/icon700.png" height= "58px">`}
+          if (response.data.weather[0].id === 800)
+        {icon.innerHTML= `<img src="img/icon800.png" height= "58px">`}
+        if (response.data.weather[0].id > 800 && response.data.weather[0].id< 805)
+        {icon.innerHTML= `<img src="img/icon80X.png" height= "58px">`}
+      }
+
+    function changeGradient(response) {
+        let temperature = Math.round(response.data.main.temp);
+        let gradient = document.getElementById("gradient");
+        let github = document.getElementById("github");
+        if (temperature < 0)
+        {github.style.color = `rgba(180,200,200,1)`;
+        gradient.style.background = `linear-gradient(54deg, rgba(193,214,232,1) 0%, rgba(255,255,255,1) 58%, rgba(180,200,200,1) 100%)`;}
+        if (temperature >= 0 && temperature < 15)
+        {github.style.color = `rgba(131,131,163,1`;
+        gradient.style.background = `linear-gradient(54deg, rgba(190,194,213,1) 0%, rgba(176,198,198,1) 58%, rgba(131,131,163,1) 100%)`;}
+        if (temperature >= 15)
+        {github.style.color = `rgba(230,200,232,1)`;
+        gradient.style.background = `linear-gradient(54deg, rgba(228,195,107,1) 0%, rgba(244,242,191,1) 58%, rgba(230,200,232,1) 100%)`;}
+      }
+    
 
  function showWeather(response) {
-  let temperature = Math.round(response.data.main.temp);
-  let wind = Math.round(response.data.wind.speed);
-  let humidity = Math.round(response.data.main.humidity);
-  let currentTempValue = document.querySelector("#current-temp-value");
-  let currentWindValue = document.querySelector("#wind-speed");
-  let currentHumidityValue = document.querySelector("#humidity");
-  currentWindValue.innerHTML = `${wind}`;
-  currentTempValue.innerHTML = `${temperature}`;
-  currentHumidityValue.innerHTML = `${humidity}`;
-   }
- 
+        let temperature = Math.round(response.data.main.temp);
+        let wind = Math.round(response.data.wind.speed);
+        let humidity = Math.round(response.data.main.humidity);
+        let name = (response.data.name.toUpperCase());
+        let currentTempValue = document.querySelector("#current-temp-value");
+        let currentWindValue = document.querySelector("#wind-speed");
+        let currentHumidityValue = document.querySelector("#humidity");
+        let currentCity = document.querySelector("#current-city");
+        currentWindValue.innerHTML = `${wind}`;
+        currentTempValue.innerHTML = `${temperature}`;
+        currentHumidityValue.innerHTML = `${humidity}`;
+        currentCity.innerHTML = `${name}`;
+        changeIcon(response);
+        changeGradient(response);
+      }
+  
 function newCity(event) {
-  event.preventDefault();
-  let newCity = document.querySelector("#enter-city");
-    let currentCity = document.querySelector("#current-city");
-  if (newCity.value) {
-    currentCity.innerHTML = `${newCity.value.toUpperCase()}`;
-  } 
-  let url = `https://api.openweathermap.org/data/2.5/weather?q=${newCity.value}&appid=${apiKey}&&units=metric`;
-axios.get(url).then(showWeather);
-newCity.value=null;
-if (UrlExists(url)===false){
-  alert("Unfortunately the forecast for this city cannot be shown. Enter another city or use geolcation.");
-  currentCity.innerHTML = `Enter another city`;
-};
- }
+        event.preventDefault();
+        let newCity = document.querySelector("#enter-city");
+          let currentCity = document.querySelector("#current-city");
+        if (newCity.value) {
+          currentCity.innerHTML = `${newCity.value.toUpperCase()}`;
+        } 
+        let url = `https://api.openweathermap.org/data/2.5/weather?q=${newCity.value}&appid=${apiKey}&&units=metric`;
+       axios.get(url).then(showWeather);
+        newCity.value=null;
+        if (UrlExists(url)===false){
+        alert("Unfortunately the forecast for this city cannot be shown. Enter another city or use geolocation.");
+        currentCity.innerHTML = `ENTER ANOTHER CITY`;}
+      }
+
+function currentPosition(position) {
+        let lat = position.coords.latitude;
+        let lon = position.coords.longitude;
+        let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&&units=metric`;
+        axios.get(url).then(showWeather);
+      }
+
+function getCoordinates(event) {
+        navigator.geolocation.getCurrentPosition(currentPosition);
+      }
 
 let newCityForm = document.querySelector("#enter-city-form");
 newCityForm.addEventListener("submit", newCity);
@@ -64,6 +109,7 @@ let minutes = now.getMinutes();
   if (minutes < 10) {
     minutes = `0${minutes}`;
   }
+
 currentDay.innerHTML = `${day.toUpperCase()}`;
 currentTime.innerHTML =`${hour}:${minutes}`;
 
@@ -88,36 +134,15 @@ currentTemp.addEventListener("click", changeTempUnit);
 //let currentTempF = document.querySelector("#current-temp-f")
 //console.log(`${currentTempF}`);
 
-function currentPosition(position) {
-  let lat = position.coords.latitude;
-  let lon = position.coords.longitude;
-  let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&&units=metric`;
-  axios.get(url).then(showWeatherCoordinates);
-}
 
- function showWeatherCoordinates(response) {
-  let temperature = Math.round(response.data.main.temp);
-  let wind = Math.round(response.data.wind.speed);
-  let humidity = Math.round(response.data.main.humidity);
-  let name = (response.data.name.toUpperCase());
-  let currentTempValue = document.querySelector("#current-temp-value");
-  let currentWindValue = document.querySelector("#wind-speed");
-  let currentHumidityValue = document.querySelector("#humidity");
-  let currentCity = document.querySelector("#current-city");
-  currentWindValue.innerHTML = `${wind}`;
-  currentTempValue.innerHTML = `${temperature}`;
-  currentHumidityValue.innerHTML = `${humidity}`;
-  currentCity.innerHTML = `${name}`;
-}
-
-function getCoordinates(event) {
-  navigator.geolocation.getCurrentPosition(currentPosition);
-}
 
 let findLocation = document.querySelector("#find-location");
 findLocation.addEventListener("click", getCoordinates);
 
 navigator.geolocation.getCurrentPosition(currentPosition);
 
-
-
+/*
+To do:
+- units conversion
+- what if name of city is too long
+*/
