0546a0a0669c22a597b68a0a554dd242c3b33dcb
natwalcz <natalia.walczak@g.pl>
2023-01-05 10:14:36 +0100

    Added geolocation.

--- src/script.js
+++ src/script.js
@@ -1,3 +1,5 @@
+ let apiKey = "b9ba0314a93083136d968577c718e31d";
+ 
  function showWeather(response) {
   let temperature = Math.round(response.data.main.temp);
   let wind = Math.round(response.data.wind.speed);
@@ -17,9 +19,9 @@ function newCity(event) {
   if (newCity.value) {
     currentCity.innerHTML = `${newCity.value.toUpperCase()}`;
   } 
-  let apiKey = "b9ba0314a93083136d968577c718e31d";
-let url = `https://api.openweathermap.org/data/2.5/weather?q=${newCity.value}&appid=${apiKey}&&units=metric`;
+  let url = `https://api.openweathermap.org/data/2.5/weather?q=${newCity.value}&appid=${apiKey}&&units=metric`;
 axios.get(url).then(showWeather);
+newCity.value=null;
  }
 
 let newCityForm = document.querySelector("#enter-city-form");
@@ -53,18 +55,18 @@ currentTime.innerHTML =`${hour}:${minutes}`;
 
 function changeTempUnit(event) {
 event.preventDefault();
-//let currentTempValue = document.querySelector("#current-temp-value");
-//let currentTempUnit=document.querySelector("#current-temp-unit");
-//let currentTempValueF = currentTempValue*1.8+32;
-//console.log(currentTempValue);
-//console.log(currentTempValueF);
-//if (currentTempUnit.innerHTML === "C") {
-//currentTempValue.innerHTML = `${currentTempValueF}` 
-//currentTempUnit.innerHTML = `F`} 
-//else 
-//{currentTemp.innerHTML = `<a href="" id="current-temp" class="current-temp">6°<span id="current-temp-unit">C</span></a>`}
-          }
-
+/*let currentTempValue = document.querySelector("#current-temp-value");
+let currentTempUnit=document.querySelector("#current-temp-unit");
+let currentTempValueF = currentTempValue*1.8+32;
+console.log(currentTempValue);
+console.log(currentTempValueF);
+if (currentTempUnit.innerHTML === "C") {
+currentTempValue.innerHTML = `${currentTempValueF}` 
+currentTempUnit.innerHTML = `F`} 
+else 
+{currentTemp.innerHTML = `<a href="" id="current-temp" class="current-temp">6°<span id="current-temp-unit">C</span></a>`}
+*/
+}
 
 let currentTemp = document.querySelector("#current-temp");
 currentTemp.addEventListener("click", changeTempUnit);
@@ -72,6 +74,34 @@ currentTemp.addEventListener("click", changeTempUnit);
 //let currentTempF = document.querySelector("#current-temp-f")
 //console.log(`${currentTempF}`);
 
+function currentPosition(position) {
+  let lat = position.coords.latitude;
+  let lon = position.coords.longitude;
+  let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&&units=metric`;
+  axios.get(url).then(showWeatherCoordinates);
+}
+
+ function showWeatherCoordinates(response) {
+  let temperature = Math.round(response.data.main.temp);
+  let wind = Math.round(response.data.wind.speed);
+  let humidity = Math.round(response.data.main.humidity);
+  let name = (response.data.name.toUpperCase());
+  let currentTempValue = document.querySelector("#current-temp-value");
+  let currentWindValue = document.querySelector("#wind-speed");
+  let currentHumidityValue = document.querySelector("#humidity");
+  let currentCity = document.querySelector("#current-city");
+  currentWindValue.innerHTML = `${wind}`;
+  currentTempValue.innerHTML = `${temperature}`;
+  currentHumidityValue.innerHTML = `${humidity}`;
+  currentCity.innerHTML = `${name}`;
+}
+
+function getCoordinates(event) {
+  navigator.geolocation.getCurrentPosition(currentPosition);
+}
+
+let findLocation = document.querySelector("#find-location");
+findLocation.addEventListener("click", getCoordinates);
 
 
 
